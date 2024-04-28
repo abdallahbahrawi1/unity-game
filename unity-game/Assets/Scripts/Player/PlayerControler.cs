c79027c7011fa79af7bc3bb152afcec208a731aa
abdallahbahrawi1 <abdallah.bahrawi400@gmail.com>
2024-04-28 03:51:14 +0300

    Adding Touching Direction script to check if player is toching the celling or the wall
    refactoring the code, and implementing input manager to control the player movement, make the player fall insted of stucking on the wall

--- unity-game/Assets/Scripts/Player/PlayerControler.cs
+++ unity-game/Assets/Scripts/Player/PlayerControler.cs
@@ -6,101 +6,94 @@ using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerControler : MonoBehaviour
 {
-
-
-    [SerializeField] Transform groundCheck;
-    bool isGrounded = false;
     int jumpCount = 1;
-    float jumpforce = 11f;
-    [SerializeField] LayerMask groundLayer;
-    [SerializeField]float dashSpeed = 20f;
-    [SerializeField]bool isDashing = false;
-    [SerializeField]float dashTime = 0f;
+    [SerializeField] float dashSpeed = 20f;
+    [SerializeField] float dashTime = 0f;
     float startDashTime = 0.2f;
-
-
-
-
     private Vector2 _moveInput;
-    [SerializeField] Rigidbody2D _rb;
-    Animator _animator;
+    [SerializeField] Rigidbody2D rb;
+    TouchingDirections touchingDirections;
+    Animator animator;
     [SerializeField]
     private bool _isMoving = false; 
-    [SerializeField]
+    [SerializeField] 
+    private bool _isDashing = false;
+    // [SerializeField]
     // private bool _isIdle = false; 
+    public float jumpforce = 11f;
     public float walkSpeed = 5f;
+
+
+    public float CurrentMoveSpeed {
+        get {
+            if(IsMoving && !touchingDirections.IsOnWall){
+                if(IsDashing){
+                    return dashSpeed;
+                }else{
+                    return walkSpeed;
+                }
+            }else {
+                return 0;
+            }
+        }
+
+    }
     public bool IsMoving { 
         get {
             return _isMoving;
         }
         private set{
             _isMoving = value;
-            _animator.SetBool(AnimationStrings.isMoving, value);   
+            animator.SetBool(AnimationStrings.isMoving, value);   
         }
     }
-    public bool isfacingRight = true;
+    public bool IsDashing { 
+        get {
+            return _isDashing;
+        }
+        private set{
+            _isDashing = value;
+            animator.SetBool(AnimationStrings.isMoving, value);   
+        }
+    }
+    private bool _isfacingRight = true;
 
     public bool IsFacingRight  { 
         get {
-            return isfacingRight ;
+            return _isfacingRight ;
         }
         private set{
-            if(isfacingRight != value){ 
+            if(_isfacingRight != value){ 
                 transform.localScale *= new Vector2(-1, 1);
             }
-            isfacingRight = value;
+            _isfacingRight = value;
         }
     }
 
     private void Awake() {
-        _rb = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        
-        //Dashing on left-shift click
-        if(Input.GetKeyDown("left shift")){
-            if(!isDashing){
-                walkSpeed += dashSpeed;
-                isDashing = true;
-                dashTime = startDashTime;
-            }
-        }
-        //Dash timing
-        if(dashTime<=0&&isDashing){
-            isDashing = false;
-            walkSpeed -= dashSpeed;
-        }else if(dashTime>0){
+    {        
+        if(dashTime <= 0 && _isDashing){
+            _isDashing = false;
+        }else if(dashTime > 0){
             dashTime-= Time.deltaTime;
         }
-        
-        //Jumping logic
-       isGrounded=Physics2D.BoxCast(groundCheck.position,new Vector2(2.5f,0.4f),0f,new Vector2(0f,0f),0.1f,groundLayer);
-       if(isGrounded)jumpCount=1;
-       
-        if(Input.GetButtonDown("Jump")&&(isGrounded||jumpCount<2)){
-              _rb.velocity= new Vector2(_rb.velocity.x,jumpforce);
-              jumpCount++;    
-        }
-        ////////////////
-        _rb.velocity = new Vector2(_moveInput.x * walkSpeed, _rb.velocity.y);
+        rb.velocity = new Vector2(_moveInput.x * CurrentMoveSpeed, rb.velocity.y);
     }
-    
 
     private void FixedUpdate() {
-
-
         
     }
 
@@ -109,8 +102,21 @@ public class PlayerControler : MonoBehaviour
         IsMoving = _moveInput != Vector2.zero;
         SetFacingDirection(_moveInput); 
     }
+
+    public void OnDash(InputAction.CallbackContext context){
+        if(context.started && !_isDashing){
+            _isDashing = true;
+            dashTime = startDashTime;
+        }
+    }
+
     public void OnJump(InputAction.CallbackContext context){
-        
+        //TODO CHECK IF ALIVE AS WELL
+        if(context.started && touchingDirections.IsGrounded){
+            animator.SetTrigger(AnimationStrings.jump);
+            rb.velocity= new Vector2(rb.velocity.x, jumpforce);
+            jumpCount = 2;
+        }
     }
 
     private void SetFacingDirection(Vector2 moveInput)
---
+++ unity-game/Assets/Scripts/Player/TouchingDirections.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchingDirections : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    CapsuleCollider2D touchingCol;
+    Animator animator;
+    public ContactFilter2D castFilter;
+    public float groundDistance = 0.05f;
+    public float wallDistance = 0.2f;
+    public float ceilingDistance = 0.05f;
+    RaycastHit2D[] groundHits = new RaycastHit2D[5];
+    RaycastHit2D[] wallHits = new RaycastHit2D[5];
+    RaycastHit2D[] ceilngHits = new RaycastHit2D[5];
+
+    [SerializeField]
+    private bool _isGrounded = true;
+    public bool IsGrounded { 
+        get {
+            return  _isGrounded;
+        } 
+        private set{
+            _isGrounded = value;
+            animator.SetBool(AnimationStrings.isGrounded, value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isOnWall= true;
+    public bool IsOnWall { 
+        get {
+            return  _isOnWall;
+        } 
+        private set{
+            _isOnWall = value;
+        }
+    }
+    private Vector2 wallCheckDirection => gameObject.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+
+    [SerializeField]
+    private bool _isOnCeiling = true;
+
+    public bool IsOnCeiling { 
+        get {
+            return  _isOnCeiling;
+        } 
+        private set{
+            _isOnCeiling = value;
+        }
+    }
+
+    private void Awake() {
+        touchingCol = GetComponent<CapsuleCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate(){
+        IsGrounded = touchingCol.Cast(Vector2.down, castFilter, groundHits , groundDistance) > 0 ;
+        // you can set the walking speed to zero to make the player fall or allow it to do wall jump or climbing 
+        IsOnWall = touchingCol.Cast(wallCheckDirection, castFilter, wallHits , wallDistance) > 0 ;
+        IsOnCeiling = touchingCol.Cast(Vector2.up, castFilter, ceilngHits , ceilingDistance) > 0 ;
+    }
+}
